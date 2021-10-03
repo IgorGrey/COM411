fb6603e46899a0e8f2dc62ed6b0ae2b76c5a6912
IgorGrey <5kanchi95@solent.ac.uk>
2021-10-03 15:24:24 +0100

    Changed code to represent data types properly *working version*

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -1,21 +1,16 @@
 # Ask user to enter their name
 print("What is your name human?")
-name = input()
+name = str(input())
 # Ask user to enter their age
 print("How old are you?")
-age = input()
+age = int(input())
 # Ask user to enter their height in meters
 print("How tall are you? (m)")
-height = input()
+height = float(input())
 # Ask user to enter their weigh in kilograms
 print("How much do you weigh? (kg)")
-weight = input()
+weight = float(input())
 # Bmi calculation (weight divided on height squared)
-bmi = weight/height**2
+bmi = weight / height ** 2
 # Show user personalised message with bmi index
 print(f"{name}, your bmi index is {bmi}")
-
-#Traceback (most recent call last):
-  File "C:\Users\maili\PycharmProjects\COM411\basics\input\data_types.py", line 14, in <module>
-    bmi = weight/height*2
-TypeError: unsupported operand type(s) for /: 'str' and 'str'
