f89bc689f5e43ae1910430b3c32aab45987e3df3
IgorGrey <5kanchi95@solent.ac.uk>
2021-10-03 15:47:46 +0100

    Added code to demonstrate the use of string operators.

---
+++ basics/input/string_operators.py
@@ -0,0 +1,12 @@
+print("What is your current lives count?")
+lives = int(input())
+print("What is your current energy level?")
+energy = int(input())
+print("what is your current shoeld level?")
+shield = int(input())
+
+print("Health has been set.")
+
+print(f"Lives:  {'♥' * lives}")
+print(f"Energy: {'♦' * energy}")
+print(f"Shield: {'♦' * shield}")
