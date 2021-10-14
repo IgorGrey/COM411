0201a0e693593f733cd2f607757d7fd422c489dd
Igor Kanchukovskyi <5kanci95@solent.ac.uk>
2021-10-14 12:45:41 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,7 @@
+def indetify():
+    print("What lies before us?")
+    obj = input()
+    if obj == ("a large boulder"):
+        print("Its time to run!")
+    else:
+        print("We will be fine.")
