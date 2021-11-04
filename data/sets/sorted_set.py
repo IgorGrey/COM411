3a2ba5122c8cff40d1170f3fdd6dba54381b69c4
IgorGrey <5kanchi95@solent.ac.uk>
2021-11-04 21:43:30 +0000

    started  to add code to form sorted set demo

---
+++ data/sets/sorted_set.py
@@ -0,0 +1,8 @@
+def observed():
+    observations = []
+    observations.append(input())
+    return observations
+
+def remove_observations(observations):
+    print("Do you wish to remove entry, y/n?")
+    while
