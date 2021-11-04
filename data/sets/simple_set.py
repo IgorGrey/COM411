4f4f62ffe2f353c71febc4510b11a266a3e5c86f
Igor Kanchukovskyi <5kanci95@Solent.ac.uk>
2021-11-04 11:22:05 +0000

    corrected typos

---
+++ data/sets/simple_set.py
@@ -0,0 +1,9 @@
+def observed():
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+def run():
+    observed()
+
+if __name__ == "__main__":
+    run()
