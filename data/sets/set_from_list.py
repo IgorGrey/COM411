69d500fcc455ccc51adc52796a12d0b1e5149055
IgorGrey <5kanchi95@solent.ac.uk>
2021-11-06 09:49:50 +0000

    Added code to demonstrate how to create a set from a list.

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -1,18 +1,25 @@
 def observed():
     observations = []
+
     for count in range(7):
         print("Enter object observed:")
         observations.append(input())
+
     return observations
+
 
 def run():
     print("Counting observation...")
     returned_observations = observed()
+
     set1 = set()
     for each_value in returned_observations:
         data = (each_value, returned_observations.count(each_value))
         set1.add(data)
-    print(set1)
+
+    for smthelse in set1:
+        print(f"{smthelse[0]} observed {smthelse[1]} times.")
+
 
 if __name__ == "__main__":
     run()
