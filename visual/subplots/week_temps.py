f4aae284469f6d0aa5bdcb162ebbbae9ca3c7eeb
Igor Kanchukovskyi <5kanci95@solent.ac.uk>
2021-11-18 11:30:53 +0000

    working version

--- visual/subplots/week_temps.py
+++ visual/subplots/week_temps.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 
 def read_data():
-    with open('visual/subplots/temps.csv') as file:
+    with open('temps.csv') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)
         data = {'week1': [], 'week2': []}
@@ -16,8 +16,8 @@ def read_data():
 def run():
     data = read_data()
 
-    fig, (ax1, ax2) = plt.subplot(1, 2)
-    ax1.plot(range(len(data['week1'])), data['weel1'])
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(range(len(data['week1'])), data['week1'])
     ax2.plot(range(len(data['week2'])), data['week2'])
     plt.show()
 
