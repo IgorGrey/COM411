3426ff915e7d4844d48b0358680249d3dce488d7
IgorGrey <5kanchi95@solent.ac.uk>
2021-11-22 16:05:22 +0000

    added more code, unfinished

--- visual/subplots/titanic.py
+++ visual/subplots/titanic.py
@@ -5,7 +5,12 @@ def read_data():
     with open('titanic.csv') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)
-        data = {'survived': [], 'sex': [], 'age': [], 'fare': []}
+        data = {
+            'survived': [],
+            'sex': [],
+            'age': [],
+            'fare': []
+        }
 
         for row in csv_reader:
             if not has_empty_values(row):
@@ -16,10 +21,22 @@ def read_data():
 
     return data
 
+# User function designed to check for empty string list
 def has_empty_values(list_to_check):
     for item in list_to_check:
         if item == "":
             return True
     return False
 
-print(read_data())
+def run():
+    data = read_data()
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(2, 2)
+    ax1.plot(data)
+    ax2.plot(data)
+    ax3.plot(data)
+    ax4.plot(data)
+    plt.show()
+
+if __name__ == "__main__":
+    run()
