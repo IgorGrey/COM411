d297290937c4148546348fe02d86b6ff0b4a8b55
Igor Kanchukovskyi <5kanci95@solent.ac.uk>
2021-11-25 12:33:19 +0000

    Added code to display a simple animation, UNFINISHED

--- visual/animatins/simple.py
+++ visual/animatins/simple.py
@@ -3,20 +3,23 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
+
 def animate(frame):
 
+    global ax
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.plot(frame, frame)
+    return
 
 
 def run():
-
     global ax
-
-    x = [2, 4, 6, 8, 10]
-    y = [2, 4, 6, 8, 10]
-    ax.plot(x(frame), y(frame))
-
-    some_animation = animation.FuncAnimation(fig, animate, frames=10, interval=100)
+    some_animation = animation.FuncAnimation(fig, animate, frames=10, interval=500)
     plt.show()
+
 
 if __name__ == "__main__":
     run()
